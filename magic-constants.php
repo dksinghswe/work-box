ff9941308117944ac10b587a4676cef282e8e06c
dksinghswe <dksinghswe@gmail.com>
2025-06-29 23:10:41 +0530

    Revert "Changes done"
    This reverts commit 1ee9f96d2596b696e88d3d404f8eb3da5822c182.

--- magic-constants.php
+++ magic-constants.php
@@ -1,17 +1,17 @@
 <?php
-// php magic constants 
+// php magic constants
 namespace php\Test;
 class Test{
 	public function __construct(){
-		echo "File -- ".__FILE__."<br>";
-		echo "Dir -- ".__DIR__."<br>";
-		echo "Class -- ".__CLASS__."<br>";
-		echo "Class -- ".Test::class."<br>";
-		echo "Function -- ".__FUNCTION__."<br>";
-		echo "Line -- ".__LINE__."<br>";
-		echo "Namespace -- ".__NAMESPACE__."<br>";
-		echo "Trait -- ".__TRAIT__."<br>";
-		echo "Method -- ".__METHOD__."<br>";
+		echo "File - ".__FILE__."<br>";
+		echo "Dir - ".__DIR__."<br>";
+		echo "Class - ".__CLASS__."<br>";
+		echo "Class - ".Test::class."<br>";
+		echo "Function - ".__FUNCTION__."<br>";
+		echo "Line - ".__LINE__."<br>";
+		echo "Namespace - ".__NAMESPACE__."<br>";
+		echo "Trait - ".__TRAIT__."<br>";
+		echo "Method - ".__METHOD__."<br>";
 	}
 }
 new Test();
